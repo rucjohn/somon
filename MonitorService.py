5fe82cf6499197a40a2fe5e0cea90d007f531a8d
rucjohn <rucjohn@163.com>
2020-12-02 07:46:34 +0800

    Add: 增加 Windows Service

---
+++ MonitorService.py
@@ -0,0 +1,45 @@
+# _*_ coding:utf-8 _*_
+
+import os
+import sys
+import time
+import winerror
+import win32timezone
+import win32event
+import win32service
+import win32serviceutil
+import servicemanager
+
+
+class SoMonService(win32serviceutil.ServiceFramework):
+
+    _svc_name_ = 'SoMoService'
+    _svc_display_name_ = 'SoMoService'
+    _svc_description_ = 'Salt Monitor Service'
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        self.isAlive = True
+
+    def SvcDoRun(self):
+        while self.isAlive:
+            time.sleep(5)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        self.isAlive = False
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        win32serviceutil.HandleCommandLine(SoMonService)
+    try:
+        evt_src_dll = os.path.abspath(servicemanager.__file__)
+        servicemanager.PrepareToHostSingle(SoMonService)
+        servicemanager.Initialize('SoMonService', evt_src_dll)
+        servicemanager.StartServiceCtrlDispatcher()
+    except win32service.error as details:
+        if details[0] == winerror.ERROR_FAILED_SERVICE_CONTROLLER_CONNECT:
+            win32serviceutil.usage()
