cf59e55ab523a20f51217360abb1f8cf1702a55b
rucjohn <rucjohn@163.com>
2020-12-12 15:18:27 +0800

    Update: 增加获取缓存目录、时间信息和遍历目录文件通用函数

--- collector/utils/common.py
+++ collector/utils/common.py
@@ -1,8 +1,11 @@
 # _*_ coding:utf-8 _*_
 
+import os
 import json
 import time
+import datetime
 import hashlib
+from collector.utils import config
 
 
 def ordered_dict():
@@ -31,3 +34,38 @@ def gen_code(data):
     except:
         md5.update('')
     return md5.hexdigest()
+
+
+def get_cache_dir(sub_name):
+    path = os.path.join(config.CACHE_DIR, sub_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+def get_time_info(fmt=None):
+    timestamp = int(time.time())
+    dt_str = None
+    if fmt:
+        dt = datetime.datetime.fromtimestamp(timestamp)
+        dt_str = dt.strftime(fmt)
+    return timestamp, str(dt_str)
+
+
+def iter_dir(path, suffix=None, exclude=[]):
+    result = list()
+    f_exclude = exclude
+    if suffix:
+        f_suffix = '.' + suffix
+    if not os.path.isdir(path):
+        return result
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if suffix:
+                if f.lower() in f_exclude:
+                    continue
+                if f.endswith(f_suffix):
+                    result.append(f)
+            else:
+                result.append(f)
+    return result
