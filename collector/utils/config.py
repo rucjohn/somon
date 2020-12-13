5040036b6e641af9d132f046150508b2f3846d56
rucjohn <rucjohn@163.com>
2020-12-13 09:11:50 +0800

    Update: 取消配置文件映射，修改为直接读取

--- collector/utils/config.py
+++ collector/utils/config.py
@@ -1,29 +1,13 @@
 # _*_ coding:utf-8 _*_
 
-from __future__ import absolute_import, unicode_literals
-import os
-import configparser
-
-
-path = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'config',
-    'config.ini'
-)
-
-cf = configparser.ConfigParser()
-cf.read(path)
-
 # cache
-CACHE_DIR = cf.get('cache', 'cache_dir')
-
+CACHE_DIR = 'C:\\COLLECT'
 # log
-LOG_DIR = cf.get('log', 'log_dir')
-LOG_VERBOSE = cf.get('log', 'verbose')
-
+LOG_DIR = 'C:\\COLLECT\\LOGS'
+LOG_VERBOSE = 'INFO'
 # db
-DB_NAME = cf.get('db', 'name')
-
+DB_NAME = 'job.db'
 # screen
-SCREEN_WIDTH = cf.getint('screen', 'width')
-SCREEN_HEIGHT = cf.getint('screen', 'height')
+SCREEN_WIDTH = 2560
+SCREEN_HEIGHT = 1440
+
